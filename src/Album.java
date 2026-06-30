ee13a2dc46efeeff7b6119753dfaddd1c710b4b2
DAVI GABRIEL TEIXEIRA DE OLIVEIRA <0123276@ifmg.edu.br>
2026-06-30 10:38:53 -0300

    Implementei o menu principal, a inserção do arquivo do próprio usuário e o método de registrar figurinha

--- src/Album.java
+++ src/Album.java
@@ -1,4 +1,5 @@
 import java.io.*; //Utilização das classes de leitura de arquivos
+import java.util.Scanner;
 
 public class Album {
     static int linhas; //declarando variáveis globais 
@@ -7,13 +8,23 @@ public class Album {
     static int[][] matriz;
 
     //=======================
-    // Método de leitura do arquivo
+    // Metodo de leitura do arquivo
     //=======================
+
+    static void cadastrarFigurinha(int selecao, int jogador) {
+        if (matriz[selecao][jogador] == 0){
+            matriz[selecao][jogador] = 1;
+            System.out.println("Figurinha nova colada!");
+        }else {
+            matriz[selecao][jogador] ++;
+            System.out.println("Figurinha repitida registrada");
+        }
+    }
     static void carregarAlbum(String caminhoArquivo) throws IOException {
         try (BufferedReader br = new BufferedReader(new FileReader(caminhoArquivo))) {
 
             // 1ª linha: dimensões
-            //Lê uma linha do arquivo com o método da classe bufferedReader br e divide strings diferentes utilizando o split
+            //Lê uma linha do arquivo com o metodo da classe bufferedReader br e divide strings diferentes utilizando o split
             String[] dimensoes = br.readLine().trim().split("\\s+");
             linhas  = Integer.parseInt(dimensoes[0]); //converte a string para inteiro
             colunas = Integer.parseInt(dimensoes[1]); //converte a string para inteiro
@@ -24,7 +35,7 @@ public class Album {
             // próximas M linhas: nomes das seleções
             for (int i = 0; i < linhas; i++) {
                 selecoes[i] = br.readLine().trim(); //Lê os nomes de cada seleção.
-                //o método trim serve para remover espaços em branco antes e depois da string lida
+                //o metodo trim serve para remover espaços em branco antes e depois da string lida
             }
 
             // próximas M linhas: valores da matriz que representam as figurinhas
@@ -42,7 +53,7 @@ public class Album {
     }
 
     //=======================
-    // Método para imprimir o álbum atual
+    // Metodo para imprimir o álbum atual
     //=======================
     static void exibirMatriz() {
         if (matriz == null) {
@@ -73,20 +84,108 @@ public class Album {
     }
 
     //=======================
-    // Método main
+    // Metodo main
     //=======================
     public static void main(String[] args) {
 
-        try {
-            carregarAlbum("Z:\\AlbumTxt\\album.txt");
-            exibirMatriz();
-        } catch (FileNotFoundException e) {
-            System.out.println("Arquivo não encontrado: " + e.getMessage());
-        } catch (NumberFormatException e) {
-            System.out.println("Formato inválido no arquivo: " + e.getMessage());
-        } catch (IOException e) {
-            System.out.println("Erro de leitura: " + e.getMessage());
-        }
+            // Menu principal
+
+            int codigo;
+            Scanner in = new Scanner(System.in);
+            System.out.println("1. Carregar álbum a partir de arquivo (fornecerei o método de leitura de arquivos) ");
+            System.out.println("2. Registrar nova figurinha");
+            System.out.println("3. Listar figurinhas faltantes");
+            System.out.println("4. Listar figurinhas repetidas");
+            System.out.println("5. Comparar dois álbuns (verificar trocas possíveis)");
+            System.out.println("6. Sair");
+
+            codigo = in.nextInt();
+            in.nextLine();
+
+            //Validação de erro para o código
+
+            while(codigo<1 || codigo>6){
+                System.out.println("Digite um código válido: ");
+                System.out.println("1. Carregar álbum a partir de arquivo (fornecerei o método de leitura de arquivos) ");
+                System.out.println("2. Registrar nova figurinha");
+                System.out.println("3. Listar figurinhas faltantes");
+                System.out.println("4. Listar figurinhas repetidas");
+                System.out.println("5. Comparar dois álbuns (verificar trocas possíveis)");
+                System.out.println("6. Sair");
+                codigo = in.nextInt();
+                in.nextLine();
+            }
+
+            //Loop de execuções que pode chamar todos os métodos
+
+            while (codigo!= 6){
+                if (codigo ==1){
+                    String caminho;
+                    System.out.println("Informe o caminho que está o arquivo em seu computador: ");
+                    caminho = in.nextLine();
+
+                    try{
+                        carregarAlbum(caminho);
+                        exibirMatriz();
+                    } catch (FileNotFoundException e) {
+                    System.out.println("Arquivo não encontrado: " + e.getMessage());
+                    } catch (NumberFormatException e) {
+                    System.out.println("Formato inválido no arquivo: " + e.getMessage());
+                    } catch (IOException e) {
+                    System.out.println("Erro de leitura: " + e.getMessage());
+                    }
+
+                    // caminho no meu pc == "Z:\\AlbumTxt\\album.txt"
+
+                } else if (codigo ==2) {
+                    if (selecoes==null){
+                        System.out.println("O albúm não foi carregado. Carregue o álbum primeiro para depois registrar a figurinha.");
+                    }else{
+                        int selecao,jogador;
+                        System.out.println("Informe o índice da seleção: ");
+                        selecao = in.nextInt();
+                        in.nextLine();
+                        System.out.println("Informe o índice da jogador: ");
+                        jogador = in.nextInt();
+                        in.nextLine();
+                        while((selecao>selecoes.length|| selecao<0)||(jogador>colunas||jogador<0)){
+                            System.out.println("Índices inválidos!");
+                            System.out.println("Informe o índice da seleção: ");
+                            selecao = in.nextInt();
+                            in.nextLine();
+                            System.out.println("Informe o índice da jogador: ");
+                            jogador = in.nextInt();
+                            in.nextLine();
+                        }
+
+                        cadastrarFigurinha(selecao,jogador);
+                        exibirMatriz();
+                    }
+
+
+                } else if (codigo==3) {
+
+                } else if (codigo ==4) {
+
+                } else if (codigo == 5){
+
+                }else{
+                    in.close();
+                }
+                System.out.println("\n");
+                System.out.println("1. Carregar álbum a partir de arquivo (fornecerei o método de leitura de arquivos) ");
+                System.out.println("2. Registrar nova figurinha");
+                System.out.println("3. Listar figurinhas faltantes");
+                System.out.println("4. Listar figurinhas repetidas");
+                System.out.println("5. Comparar dois álbuns (verificar trocas possíveis)");
+                System.out.println("6. Sair");
+
+                codigo = in.nextInt();
+                in.nextLine();
+            }
+
+
+
 
     }
 }
